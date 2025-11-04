3f5c3614ddebbcb9445ac0e9a427b6249eecd5f1
vishal23S <vsunkara@nyayacsr.com>
2025-11-04 06:47:45 -0800

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ElectroShoeOnePager from "./ElectroShoeOnePager";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ElectroShoeOnePager />
+  </React.StrictMode>
+);
+
