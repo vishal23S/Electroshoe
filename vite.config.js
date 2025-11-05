be216f3d366fa364cac91e81d5806ff6481fbacd
vishal23S <vsunkara@nyayacsr.com>
2025-11-05 03:57:07 -0800

    new changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Electroshoe/', // ✅ must match your repo name exactly
+  base: '/', // ✅ must match your repo name exactly
 })
 
